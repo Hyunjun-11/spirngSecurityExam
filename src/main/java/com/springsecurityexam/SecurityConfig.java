2fd7a9389ab27e84ab9394c8950e3fb6a36d186b
Hyunjun-11 <mical0108@gmail.com>
2024-04-22 23:45:34 +0900

    securityConfig

--- src/main/java/com/springsecurityexam/SecurityConfig.java
+++ src/main/java/com/springsecurityexam/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.springsecurityexam;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth ->auth.anyRequest().authenticated())
+
                 .formLogin(form->form
                         //사용자 정의 로그인 페이지로 전환, 기본페이지 무시
                         .loginPage("/loginPage")
