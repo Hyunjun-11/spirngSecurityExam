c7a318ea7ad89814c54acb7d37ad8610c73e5ea5
Hyunjun-11 <mical0108@gmail.com>
2024-05-01 12:09:44 +0900

    fix: httpbasic

---
+++ src/main/java/com/springsecurityexam/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package com.springsecurityexam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setHeader("WWW-Authenticate", "Basic realm=security");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    }}
--- src/main/java/com/springsecurityexam/SecurityConfig.java
+++ src/main/java/com/springsecurityexam/SecurityConfig.java
@@ -4,7 +4,6 @@ package com.springsecurityexam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -22,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth ->auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .httpBasic(basic ->basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
 
         return http.build();
