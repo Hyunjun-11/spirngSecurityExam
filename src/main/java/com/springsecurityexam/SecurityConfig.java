754a3909ea78d66841bcf23d83f9af0cac3c82e4
Hyunjun-11 <mical0108@gmail.com>
2024-05-07 01:30:01 +0900

    docs: securityConfig

--- src/main/java/com/springsecurityexam/SecurityConfig.java
+++ src/main/java/com/springsecurityexam/SecurityConfig.java
@@ -25,11 +25,17 @@ public class SecurityConfig {
                 .authorizeRequests(auth -> auth.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .rememberMe(rememberMe -> rememberMe
+                        //기억하기 매개변수가 설정되지 않았을때에도 쿠키가 항상 생성되어야 하는지 여부
                         .alwaysRemember(true)
+                        //토큰이 유요한 시간을 지정할수있다.
                         .tokenValiditySeconds(3600)
+                        //UserDetails를 조회하기위해 사용되는 UserDetailsService를 지정한다.
                         .userDetailsService(userDetailsService())
+                        //로그인시 사용자를 기억하기위해 사용되는 매개변수이며 기본값은 remember-me 이다
                         .rememberMeCookieName("remember")
+                        //토큰을 저장하는 이름이며 기본값은 remember-me 이다
                         .rememberMeParameter("remember")
+                        //기억하기인증을 위해 토큰을 식별하는 키 설정
                         .key("security")
 
                 )
